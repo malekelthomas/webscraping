180372fda3c79a89919e539a2a862ed9be6ecab7
leke <malekelthomas97@gmail.com>
2021-05-23 20:29:23 -0400

    get url and name from cl

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+import requests as req
+import parsers as p
+import sys
 
+def main():
+    
+    url, shoeName = sys.argv[1], sys.argv[2]
+    r = req.get(url)
+    savedImgs = p.save_product_images_sg(r, shoeName) #save product images
+    
 
+if __name__ == "__main__":
+    main()

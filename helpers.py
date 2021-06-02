f8f6df8f004ae8bacb30c4a37b8c848717efe5b8
leke <malekelthomas97@gmail.com>
2021-06-02 01:17:40 -0400

    gen_sku, store_shoe

--- helpers.py
+++ helpers.py
@@ -1,5 +1,10 @@
 import os, os.path
 import errno
+import time
+import uuid
+from bs4 import BeautifulSoup
+import requests as req
+import parsers as p
 
 # Taken from https://stackoverflow.com/a/600612/119527
 def mkdir_p(path):
@@ -21,3 +26,37 @@ def safe_open_wb(path): #write bytes
     '''
     mkdir_p(os.path.dirname(path))
     return open(path, 'wb')
+
+def gen_sku(name):
+    skuNum = str(uuid.uuid1()).split("-")
+    sku = ""
+    splitName = name.split('-')
+    for i in range(len(splitName)):
+        if splitName[i] != '':
+            sku+=splitName[i][0]
+    sku+=skuNum[0].upper()
+    return sku
+
+def store_shoe(url: str, shoeName: str):
+    res = req.get(url)
+    soup = BeautifulSoup(res.content, 'lxml')
+    imgs, name = p.save_product_images_sg(soup, shoeName) #get product images
+    sizesPrices = p.save_prices_sg(soup) #get product prices
+    releaseDate = p.save_release_date_sg(soup)
+    sku = gen_sku(name)
+    j = {
+        "brand":"Jordan",
+        "model":name,
+        "sku":sku,
+        "photos":imgs,
+        "site_size_price": {
+            "sites_sizes_prices":{
+                "STADIUMGOODS":
+                    {
+                        "sizes_prices":sizesPrices
+                        }
+                    }
+            },
+        "release_date": releaseDate
+        }
+    res = req.post('http://localhost:8080/sneakers/', json=j)
